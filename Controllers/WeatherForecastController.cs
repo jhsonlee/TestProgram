733e219487cddb9a13a92306699f1b1f2c49f185
Haskin <haskinsgu@aslcn.com.cn>
2022-01-06 17:03:15 +0800

    1

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,15 +11,7 @@ namespace WebAPI.Controllers {
     public class WeatherForecastController : ControllerBase {
         private static readonly string[] Summaries = new [] {
             "Freezing",
-            "Bracing",
-            "Chilly",
-            "Cool",
-            "Mild",
-            "Warm",
-            "Balmy",
-            "Hot",
-            "Sweltering",
-            "Scorching"
+
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
