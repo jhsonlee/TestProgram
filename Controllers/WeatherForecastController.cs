30d787268e6958212a0a2b7c3543567b77eedcf3
Haskin <haskinsgu@aslcn.com.cn>
2022-01-07 14:17:21 +0800

    123

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,7 +11,15 @@ namespace WebAPI.Controllers {
     public class WeatherForecastController : ControllerBase {
         private static readonly string[] Summaries = new [] {
             "Freezing",
-
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -24,8 +32,8 @@ namespace WebAPI.Controllers {
         public IEnumerable<WeatherForecast> Get () {
             var rng = new Random ();
             return Enumerable.Range (1, 5).Select (index => new WeatherForecast {
-
-                    TemperatureC = rng.Next (-20, 55),
+                    Date = DateTime.Now.AddMonths (index),
+                        TemperatureC = 0001,
                         Summary = Summaries[rng.Next (Summaries.Length)]
                 })
                 .ToArray ();
